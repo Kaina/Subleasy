f051fe6fcde8adc32c74447908234d0d94982e3a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-21 14:40:51 -0500

    create listing factory and spec

--- spec/models/lessee_spec.rb
+++ spec/models/lessee_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
+<<<<<<< HEAD
 
 describe Lessee do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+=======
+>>>>>>> create listing factory and spec
---
+++ spec/models/listing_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Listing do 
+  let(:listing) { FactoryGirl.create(:listing) }
+end
